2a8ad2fe98db36df7458cfd1bff1bbf43f87c9ca
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2021-08-25 19:54:47 -0300

    Create ListInt.h

---
+++ TrabQuest1/ListInt.h
@@ -0,0 +1,20 @@
+#ifndef LISTAINT_H_INCLUDED
+#define LISTAINT_H_INCLUDED
+
+
+typedef struct lista *Lista; //sempre do tipo ponteiro ao criar TAD
+
+
+Lista cria_lista();
+int lista_vazia(Lista lst);
+int lista_cheia(Lista lst);
+int insere_elem(Lista lst, int elem);
+int remove_elem(Lista lst,int elem);
+int apaga_lista(Lista *end_lst);
+int esvazia_lista(Lista lst);
+int get_elem_pos(Lista lst, int pos, int *elem);
+int remove_negativos(Lista x);
+int remove_pares(Lista x);
+
+
+#endif // LISTAINT_H_INCLUDED
