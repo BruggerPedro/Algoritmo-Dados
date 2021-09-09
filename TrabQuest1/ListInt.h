9c2a52967912abc944ce9522c42e8bedb2ee2895
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-09 13:38:59 -0300

    Update ListInt.h

--- TrabQuest1/ListInt.h
+++ TrabQuest1/ListInt.h
@@ -1,5 +1,5 @@
-#ifndef LISTACHAR_H_INCLUDED
-#define LISTACHAR_H_INCLUDED
+#ifndef LISTAINT_H_INCLUDED
+#define LISTAINT_H_INCLUDED
 
 
 typedef struct lista *Lista; //sempre do tipo ponteiro ao criar TAD
@@ -8,21 +8,15 @@ typedef struct lista *Lista; //sempre do tipo ponteiro ao criar TAD
 Lista cria_lista();
 int lista_vazia(Lista lst);
 int lista_cheia(Lista lst);
-int insere_elem(Lista lst, char elem[]);
-int remove_elem(Lista lst,char elem[]);
-int esvazia_lista(Lista lst);
+int insere_elem(Lista lst, int elem);
+int remove_elem(Lista lst,int elem);
 int apaga_lista(Lista *end_lst);
-int get_elem_pos(Lista lst, int pos, char *elem);
-
-
-int remove_todas(Lista lst, char letra);
-int remove_maior(Lista lst);
+int esvazia_lista(Lista lst);
+int get_elem_pos(Lista lst, int pos, int *elem);
+int remove_negativos(Lista x);
+int remove_pares(Lista x);
 int tamanho_lista(Lista x, int *tamanho);
-Lista concatena_listas(Lista l1, Lista l2);
-
+Lista intercala_listas(Lista l1, Lista l2);
 void libera(Lista x);
 
-
-
-
-#endif // LISTACHAR_H_INCLUDED
+#endif // LISTAINT_H_INCLUDED
