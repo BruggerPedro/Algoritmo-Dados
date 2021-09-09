70cf23a5c427967d01548073d2f530539b39ee42
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-09 00:03:30 -0300

    Update ListaChar.h

--- TrabQuest2/ListaChar.h
+++ TrabQuest2/ListaChar.h
@@ -8,14 +8,15 @@ typedef struct lista *Lista; //sempre do tipo ponteiro ao criar TAD
 Lista cria_lista();
 int lista_vazia(Lista lst);
 int lista_cheia(Lista lst);
-int insere_elem(Lista lst, char elem);
-int remove_elem(Lista lst,char elem);
-int apaga_lista(Lista *end_lst);
+int insere_elem(Lista lst, char elem[]);
+int remove_elem(Lista lst,char elem[]);
 int esvazia_lista(Lista lst);
-int get_elem_pos(Lista lst, int pos, char *elem);
+int apaga_lista(Lista *end_lst);
+int get_elem_pos(Lista lst, int pos, char *elem[]);
+
+
 int tamanho_lista(Lista x, int *tamanho);
 Lista intercala_listas(Lista l1, Lista l2);
 void libera(Lista x);
 
 #endif // LISTACHAR_H_INCLUDED
-
