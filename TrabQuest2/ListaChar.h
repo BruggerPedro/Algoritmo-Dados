73b858998b55a3fd81e9b2c9c87c1edb0cbf3dc4
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-09 11:00:48 -0300

    Update ListaChar.h

--- TrabQuest2/ListaChar.h
+++ TrabQuest2/ListaChar.h
@@ -12,17 +12,17 @@ int insere_elem(Lista lst, char elem[]);
 int remove_elem(Lista lst,char elem[]);
 int esvazia_lista(Lista lst);
 int apaga_lista(Lista *end_lst);
-int get_elem_pos(Lista lst, int pos, char *elem[]);
+int get_elem_pos(Lista lst, int pos, char *elem);
 
-int tamanho_lista(Lista x, int *tamanho);
-void libera(Lista x);
-int get_elem_pos(Lista lst, int pos, char *n[]);
+
 int remove_todas(Lista lst, char letra);
+int remove_maior(Lista lst);
+int tamanho_lista(Lista x, int *tamanho);
+Lista concatena_listas(Lista l1, Lista l2);
+
+void libera(Lista x);
 
 
-
-
-Lista intercala_listas(Lista l1, Lista l2);
 
 
 #endif // LISTACHAR_H_INCLUDED
