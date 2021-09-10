65b41f1507048362d1e2f61ed7fd441771f1aaf6
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-09-10 15:21:50 -0300

    Update ListaFloat.h

--- TrabQuest3/ListaFloat.h
+++ TrabQuest3/ListaFloat.h
@@ -1,6 +1,16 @@
 #ifndef LISTAFLOAT_H_INCLUDED
 #define LISTAFLOAT_H_INCLUDED
 
+typedef struct no *Lista; //sempre do tipo ponteiro ao criar TAD
+
+Lista cria_lista();
+int lista_vazia(Lista lst);
+int lista_cheia(Lista li);
+int insere_elem(Lista *lst, float elem);
+int remove_elem(Lista *lst, float elem);
+int esvazia_lista(Lista *lst);
+int tamanho_lista(Lista *lst, int *tam);
+void apaga_lista(Lista *lst);
 
 
 #endif // LISTAFLOAT_H_INCLUDED
