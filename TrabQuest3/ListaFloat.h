b4467853ba4d1b55d91b256d72b05de92bc8cbcd
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-11 07:39:14 -0300

    Update ListaFloat.h

--- TrabQuest3/ListaFloat.h
+++ TrabQuest3/ListaFloat.h
@@ -3,14 +3,21 @@
 
 typedef struct no *Lista; //sempre do tipo ponteiro ao criar TAD
 
+
 Lista cria_lista();
 int lista_vazia(Lista lst);
-int lista_cheia(Lista li);
+int lista_cheia(Lista *li); // É necessario permitir ao usuario usar a função lista_cheia, mesmo sabendo que na teoria nao existe dinamica cheia?
 int insere_elem(Lista *lst, float elem);
 int remove_elem(Lista *lst, float elem);
 int esvazia_lista(Lista *lst);
-int tamanho_lista(Lista *lst, int *tam);
 void apaga_lista(Lista *lst);
+int get_elem_pos(Lista *lst, int pos, float *elem);
 
+int tamanho_lista(Lista *lst, int *tam);
+int remove_menor(Lista *lst, float *menor);
+int listas_iguais(Lista *l1, Lista *l2);
+Lista intercala_listas(Lista *l1, Lista *l2);
+
+void libera(Lista x);
 
 #endif // LISTAFLOAT_H_INCLUDED
