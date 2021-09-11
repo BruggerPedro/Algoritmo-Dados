c95b4a0f94328f20185f6d856a5dc367501e35e0
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-09-11 12:21:43 -0300

    Create ListaCiclo.h

---
+++ TrabQuest5/ListaCiclo.h
@@ -0,0 +1,11 @@
+#ifndef LISTACICLO_H_INCLUDED
+#define LISTACICLO_H_INCLUDED
+
+typedef struct no * Lista;
+Lista cria_lista();
+int lista_vazia (Lista lst);
+int insere_inicio(Lista *lst, char elem);
+char get_elem_pos(Lista lst, int pos);
+void apaga_lista(Lista *lst);
+
+#endif // LISTACICLO_H_INCLUDED
