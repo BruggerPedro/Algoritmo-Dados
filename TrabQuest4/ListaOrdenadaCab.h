25270c8e0bdcba543d8eaca7fd33a433d2f087f6
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-11 11:14:26 -0300

    Update ListaOrdenadaCab.h

--- TrabQuest4/ListaOrdenadaCab.h
+++ TrabQuest4/ListaOrdenadaCab.h
@@ -6,8 +6,19 @@ typedef struct no *Lista; //sempre do tipo ponteiro ao criar TAD
 Lista cria_lista();
 int lista_vazia(Lista lst);
 int lista_cheia(Lista *li); // É necessario permitir ao usuario usar a função lista_cheia, mesmo sabendo que na teoria nao existe dinamica cheia?
-int insere_elem(Lista *lst, float elem);
-int remove_elem(Lista *lst, float elem);
+int insere_ord(Lista *lst, int elem);
+int remove_ord(Lista *lst, int elem);
+int esvazia_lista(Lista *lst);
+void apaga_lista(Lista *lst);
+int get_elem_pos(Lista *lst, int pos, float *elem);
 
+
+int tamanho_lista(Lista *lst, int *tam);
+int remove_pos(Lista *lst, int pos, int *valor);
+Lista inverter_lista(Lista *l1);
+Lista intercala(Lista *l1,Lista *l2);
+
+
+void libera(Lista x);
 
 #endif // LISTAORDENADACAB_H_INCLUDED
