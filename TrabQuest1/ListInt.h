1b37b166cadc1277b47aaa88ebc990ab45b61c88
Anderson Alves <89470583+and19alves@users.noreply.github.com>
2021-09-13 08:45:01 -0300

    Update ListInt.h

--- TrabQuest1/ListInt.h
+++ TrabQuest1/ListInt.h
@@ -2,9 +2,9 @@
 #define LISTAINT_H_INCLUDED
 
 
-typedef struct lista *Lista; //sempre do tipo ponteiro ao criar TAD
+typedef struct lista *Lista; // "Apelidando" a struct lista para Lista (sendo um ponteiro pra struct).
 
-
+// Operações Basicas:
 Lista cria_lista();
 int lista_vazia(Lista lst);
 int lista_cheia(Lista lst);
@@ -13,6 +13,8 @@ int remove_elem(Lista lst,int elem);
 int apaga_lista(Lista *end_lst);
 int esvazia_lista(Lista lst);
 int get_elem_pos(Lista lst, int pos, int *elem);
+
+// Operações complementares:
 int remove_negativos(Lista x);
 int remove_pares(Lista x);
 int tamanho_lista(Lista x, int *tamanho);
