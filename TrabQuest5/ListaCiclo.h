d39b0908d36f336e5f486dfd146874cb93aceab3
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-09-13 15:20:57 -0300

    Update ListaCiclo.h

--- TrabQuest5/ListaCiclo.h
+++ TrabQuest5/ListaCiclo.h
@@ -5,8 +5,14 @@ typedef struct no * Lista;
 
 Lista cria_lista();
 int lista_vazia (Lista lst);
-int insere_inicio(Lista *lst, char elem);
-char get_elem_pos(Lista lst, int pos);
+int insere_final(Lista *lst, char elem);
+int remove_inicio(Lista *lst, char *elem);
+int esvazia_lista(Lista *lst);
 void apaga_lista(Lista *lst);
+char get_elem_pos(Lista lst, int pos);
+int insere_inicio(Lista *lst, char elem);
+int insere_posicao(Lista *lst,int pos, char elem);
+int remover_fim(Lista *lst,char *elem);
+int remove_vogais(Lista *lst);
 
 #endif // LISTACICLO_H_INCLUDED
