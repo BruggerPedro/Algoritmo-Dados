7ebc67cb3d091dbe2e80afde7f91588f831409af
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-09-13 15:48:53 -0300

    Update ListaDouble.h

--- TrabQuest6/ListaDouble.h
+++ TrabQuest6/ListaDouble.h
@@ -1,12 +1,17 @@
 #ifndef LISTADOUBLE_H_INCLUDED
 #define LISTADOUBLE_H_INCLUDED
 
-typedef struct no * Lista;
+typedef struct no * Lista; //"Apelidando" a struct lista para Lista (sendo um ponteiro pra struct).
+
+// Operações Basicas:
 Lista cria_lista();
-int insere_elemento(Lista *lst, int elem);
+int get_elem_pos(Lista lst, int pos);
+void apaga_lista(Lista *lst);
+
+// Operações complementares:
 int remove_elemento(Lista *lst, int elem);
 int remove_maior(Lista *lst, int *elem);
-void apaga_lista(Lista *lst);
+int insere_elemento(Lista *lst, int elem);
 Lista inverte(Lista* lst);
 
 #endif //LISTADOUBLE_H_INCLUDED
