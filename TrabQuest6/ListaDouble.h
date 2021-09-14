f3a54d06ca05b813de3a8188c398e2a9499117f8
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-09-13 21:12:45 -0300

    Update ListaDouble.h

--- TrabQuest6/ListaDouble.h
+++ TrabQuest6/ListaDouble.h
@@ -5,13 +5,21 @@ typedef struct no * Lista; //"Apelidando" a struct lista para Lista (sendo um po
 
 // Operações Basicas:
 Lista cria_lista();
-int get_elem_pos(Lista lst, int pos);
+int lista_vazia (Lista lst);
+// É necessario permitir ao usuario usar a função lista_cheia, mesmo sabendo que na teoria nao existe dinamica cheia?
+//int lista_cheia(Lista *li);
+int insere_elemento(Lista *lst, double elem);
+int remove_elemento(Lista *lst, double elem);
+int esvazia_lista(Lista *lst);
 void apaga_lista(Lista *lst);
+int get_elem_pos(Lista lst, int pos);
 
 // Operações complementares:
-int remove_elemento(Lista *lst, int elem);
-int remove_maior(Lista *lst, int *elem);
-int insere_elemento(Lista *lst, int elem);
+int remove_todos(Lista *lst, double *elem);
+int remove_maior(Lista *lst, double *elem);
+int insere_posicao(Lista *lst, int pos, double elem);
 Lista inverte(Lista* lst);
+void libera(Lista x);
+
 
 #endif //LISTADOUBLE_H_INCLUDED
